b60092e480e538ad14d44b4d6035dd049026838b
Martin Schulze <martin.schulze@e2m.de>
2025-01-17 11:15:48 +0100

    fix: run hook on changes to pyproject.toml, poetry.lock changes, and.pre-commit-hooks.yaml

--- pre_commit_mypy_poetry_sync_hook/sync_mypy_additional_dependencies.py
+++ pre_commit_mypy_poetry_sync_hook/sync_mypy_additional_dependencies.py
@@ -4,11 +4,23 @@ import ruamel.yaml
 from poetry.core.factory import Factory
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('pre_commit_config_yaml_path', type=Path)
-    parser.add_argument("--pyproject_path", type=Path, help="Path to the pyproject.toml", default=Path("pyproject.toml"))
-    parser.add_argument("--groups", nargs="*", help="List of groups to sync dependencies for")
+    parser.add_argument(
+        "filenames", nargs="*", help="Files pre-commit wants to check (we ignore this!)"
+    )
+    parser.add_argument(
+        "--pre-commit-config-yaml-path",
+        type=Path,
+        help="Path to .pre-commit-config.yaml",
+    )
+    parser.add_argument(
+        "--pyproject-path", type=Path, help="Path to the pyproject.toml"
+    )
+    parser.add_argument(
+        "--groups", nargs="*", help="List of groups to sync dependencies for"
+    )
     parser.add_argument("--yaml-map-indent", type=int, default=2)
     parser.add_argument("--yaml-sequence-indent", type=int, default=2)
     parser.add_argument("--yaml-sequence-dash-offset", type=int, default=0)
@@ -17,7 +29,21 @@ def main():
 
     _sync(**vars(args))
 
-def _sync(pre_commit_config_yaml_path: Path, pyproject_path: Path | None = None, groups: list[str] | None = None, yaml_map_indent: int = 2, yaml_sequence_indent: int = 2, yaml_sequence_dash_offset: int = 0):
+
+def _sync(
+    filenames: list[str] | None = None,
+    pre_commit_config_yaml_path: Path | None = None,
+    pyproject_path: Path | None = None,
+    groups: list[str] | None = None,
+    yaml_map_indent: int = 2,
+    yaml_sequence_indent: int = 2,
+    yaml_sequence_dash_offset: int = 0,
+):
+    if pre_commit_config_yaml_path is None:
+        pre_commit_config_yaml_path = Path.cwd() / ".pre-commit-config.yaml"
+    if pyproject_path is None:
+        pyproject_path = Path.cwd() / "pyproject.toml"
+
     yaml_config = ruamel.yaml.YAML()
     yaml_config.preserve_quotes = True
     yaml_config.map_indent = yaml_map_indent
@@ -32,17 +58,24 @@ def _sync(pre_commit_config_yaml_path: Path, pyproject_path: Path | None = None,
     if groups is None:
         groups = sorted(poetry_package.dependency_group_names())
 
-    dependencies = (dependency for group in groups for dependency in poetry_package.dependency_group(group).dependencies)
+    dependencies = (
+        dependency
+        for group in groups
+        for dependency in poetry_package.dependency_group(group).dependencies
+    )
 
     update = False
     for repo in pre_commit_config["repos"]:
         for hook in repo["hooks"]:
             if hook["id"] == "mypy":
-                hook["additional_dependencies"] = [dependency.to_pep_508() for dependency in dependencies]
+                hook["additional_dependencies"] = [
+                    dependency.to_pep_508() for dependency in dependencies
+                ]
                 update = True
 
     if update:
         yaml_config.dump(pre_commit_config, pre_commit_config_yaml_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
